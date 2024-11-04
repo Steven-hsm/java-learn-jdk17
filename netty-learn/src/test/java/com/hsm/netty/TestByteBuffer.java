cd335f27dfdab7dd552ba2ab6b649720fc247b1d
1586558083@qq.com <1586558083@qq.com>
2024-11-04 20:34:06 +0800

    netty学习第一天

---
+++ netty-learn/src/test/java/com/hsm/netty/TestByteBuffer.java
@@ -0,0 +1,39 @@
+package com.hsm.netty;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+@Slf4j
+public class TestByteBuffer {
+    public static void main(String[] args) {
+        //FileChannel
+        //1. 输入输出流
+        //2. RandomAccessFile
+
+        try(FileChannel channel = new FileInputStream("data.txt").getChannel()){
+            //准备缓冲区
+            ByteBuffer buffer = ByteBuffer.allocate(2);
+
+            while(true){
+                //从channel读取数据，向buffer写入
+                int len = channel.read(buffer);
+                if(len == -1){
+                    break;
+                }
+                //打印buffer的内容
+                buffer.flip();//切换至读模式
+                while (buffer.hasRemaining()){
+                    byte b = buffer.get();
+                    log.error("读取到的内容：{}" ,(char) b);
+                }
+                buffer.flip();
+            }
+
+
+        }catch (Exception ignored){
+
+        }
+    }
+}
---
+++ netty-learn/src/test/java/com/hsm/netty/TestByteBufferReadWrite.java
@@ -0,0 +1,29 @@
+package com.hsm.netty;
+
+import java.nio.ByteBuffer;
+
+import static com.hsm.netty.ByteBufferUtil.debugAll;
+
+public class TestByteBufferReadWrite {
+
+    public static void main(String[] args) {
+        ByteBuffer allocate = ByteBuffer.allocate(10);
+        ByteBuffer directByteBuffer = ByteBuffer.allocateDirect(10);
+        debugAll(allocate);
+        allocate.put((byte) 0x61);
+        debugAll(allocate);
+
+        allocate.put(new byte[]{0x62, 0x63, 0x64});
+        debugAll(allocate);
+
+        allocate.flip();
+        debugAll(allocate);
+
+        allocate.flip();
+        debugAll(allocate);
+
+        //allocate.put(new byte[]{0x62, 0x63, 0x64,0x62, 0x63, 0x64});
+        //debugAll(allocate);
+
+    }
+}
